b2bf8c190d714f681fc43f01a1408037a285b701
Kismet-Fate <76886771+Kismet-Fate@users.noreply.github.com>
2022-06-21 16:50:30 -0400

    Add files via upload

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,6 @@
 cd project-chad-wojak
+git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
-tmux new
-tmux send -t 0 'flask run --host=0.0.0.0' ENTER
+tmux new-session -d -s detached
+tmux send-keys 'flask run --host=0.0.0.0' C-m
