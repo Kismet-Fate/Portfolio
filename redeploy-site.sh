c27c88e1dbb701c5d0eebc93f2e827845a44ea22
Kismet-Fate <76886771+Kismet-Fate@users.noreply.github.com>
2022-06-21 16:57:03 -0400

    Add files via upload

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,7 @@
 cd project-chad-wojak
 git fetch && git reset origin/main --hard
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 tmux new-session -d -s detached
